cfd79cb2982f110b9f28ac163fc147aa6859681b
mudit <9289168+muditporwal@users.noreply.github.com>
2019-08-26 16:54:48 +0800

    Spring Boot Kotlin Sample Project Commit

---
+++ src/test/kotlin/com/tryout/kotlinkoans/KotlinkoansApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.tryout.kotlinkoans
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class KotlinkoansApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
